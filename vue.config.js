f39d827901cd82f725d5ecba8d9ef0f168e4c4eb
relzx766 <2984109304@qq.com>
2023-10-26 12:04:46 +0800

    引入xss防止v-html的xss注入风险

--- vue.config.js
+++ vue.config.js
@@ -21,6 +21,23 @@ module.exports = defineConfig({
                     use: 'babel-loader'
                 }
             ]
+        },
+        chainWebpack: config => {
+            config.module
+                .rule("vue")
+                .use("vue-loader")
+                .loader("vue-loader")
+                .tap(options => {
+                    options.compilerOptions.directives = {
+                        html(node, directiveMeta) {
+                            (node.props || (node.props = [])).push({
+                                name: "innerHTML",
+                                value: `xss(_s(${directiveMeta.value}))`
+                            });
+                        }
+                    };
+                    return options;
+                });
         }
     }
 })
