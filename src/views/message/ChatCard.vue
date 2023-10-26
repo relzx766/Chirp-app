a5e20b823b74ae5ae1c306257678d13f2816fd62
relzx766 <2984109304@qq.com>
2023-10-26 12:06:29 +0800

    Revert "样式优化"
    This reverts commit 5cc97b873e24c9b9f9c5e0393d751ffea66d57fa.

--- src/views/message/ChatCard.vue
+++ src/views/message/ChatCard.vue
@@ -1,4 +1,5 @@
 <template>
+
   <div>
     <div v-if="getChat.messages.length>0" style="height: 100vh">
       <div style="display: flex;align-items: center;border-bottom: 1px solid #DCDFE6;padding: 12px">
@@ -8,8 +9,7 @@
           </span>
       </div>
       <div class="overflow-y-auto content"
-           style="margin-bottom: 40px;min-height: 70%;max-height: 90%;display: flex;flex-direction: column-reverse;padding-top: 24px;padding-bottom: 24px"
-      ref="content" @scroll="loadMore1">
+           style="margin-bottom: 180px;height: 70vh;  display: flex;flex-direction: column-reverse;">
         <el-row v-for="(item,index) in getChat.messages" :key="item.id">
           <!--          如果消息间隔相差2分组同时不是第一条（在此为最后一条）-->
           <el-row v-if="index<getChat.messages.length-1&&subtractDates(getChat.messages[index].createTime,getChat.messages[index+1].createTime)>2*60*1000"
@@ -21,9 +21,10 @@
           </el-row>
           <message-card :message="item"
                         :reverse="item.senderId===$store.getters.getUser.id"
-                        :user="item.senderId===$store.getters.getUser.id?$store.getters.getUser:getChat.user"
-          class="mb-2 mt-2"/>
+                        :user="item.senderId===$store.getters.getUser.id?$store.getters.getUser:getChat.user"/>
+
         </el-row>
+
       </div>
     </div>
     <div class="position-fixed bottom-0 end-1"
@@ -38,24 +39,19 @@
 <script>
 import MessageCard from "@/views/message/MessageCard.vue";
 import SendCard from "@/views/message/SendCard.vue";
-import {getChatHistory, markConversationRead} from "@/api/advice";
+import {markConversationRead} from "@/api/advice";
 import {subtractDates} from "@/util/tools";
-import {chatDate, msgDate} from "@/util/formatter";
+import {chatDate, msgDate} from "../../util/formatter";
 
 export default {
   name: "ChatCard",
+  methods: {chatDate, msgDate, subtractDates},
   props: {
     conversation: String
   },
   components: {
     SendCard,
     MessageCard
-  },
-  data(){
-    return{
-      isLoading:false,
-      isBottom:false
-    }
   },
   computed: {
     getChat() {
@@ -66,37 +62,11 @@ export default {
           conversation: "",
           messages: [],
           unreadCount: 0,
-          user: {},
-          page:1
+          user: {}
         };
       }
-    },
-  },
-  methods: {chatDate, msgDate, subtractDates,
-  loadMore1(){
-    let scrollHeight = this.$refs.content.scrollHeight;
-    let clientHeight = this.$refs.content.clientHeight;
-    let scrollTop = this.$refs.content.scrollTop;
-    if (scrollTop +scrollHeight -10 <= clientHeight &&!this.isBottom&&!this.isLoading) {
-      this.isLoading=true;
-      let page=this.getChat.page
-      getChatHistory(this.getChat.user.id,page).then(res=>{
-        if (res.data.record.length<=0){
-          this.isBottom=true;
-        }else {
-          this.$store.commit('addPrivateMessage',{
-            payload:res.data.record,
-            top:false
-          });
-          this.$store.commit('setConvPage',{
-            conversation:this.getChat.conversation,
-            page:page+1
-          })
-        }
-        this.isLoading=false;
-      })
     }
-  }},
+  },
   created() {
     markConversationRead([this.conversation]);
     this.$store.commit('setConversationUnread', {conversation: this.conversation, count: 0});
@@ -127,7 +97,7 @@ export default {
   -webkit-box-shadow: inset 0 0 5px rgba(0, 0, 0, 0.2);
   border-radius: 10px;
   background: #ededed;
-  margin-top: -20px;
+  margin-top: -30px;
 }
 
 /* 滚动条两端按钮 */
